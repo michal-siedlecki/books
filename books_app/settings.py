159f2d476d230650e17bec04e5d5a7142f1ba1b4
Michal <siedlecki.michal@gmail.com>
2020-08-11 00:20:25 +0200

    hide secret key

--- books_app/settings.py
+++ books_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dfgbvewtESRfreaaaB%%$3^&__76TYj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
