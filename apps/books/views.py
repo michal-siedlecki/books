0c0d449ad4101b1b029f7bce69cc77fcaa7a9ee4
Michal <siedlecki.michal@gmail.com>
2020-08-10 12:44:05 +0200

    add authors and published date filters

--- apps/books/views.py
+++ apps/books/views.py
@@ -1,15 +1,26 @@
+from django.db.models import QuerySet
 from rest_framework import generics
-from django_filters.rest_framework import DjangoFilterBackend
-from .filters import AuthorFilter
 from .models import Book, BookSerializer
 
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
-    filter_class = AuthorFilter
-    filterset_fields = ['published_date', 'title', 'average_rating', 'ratings_count', 'authors']
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned books to a given authors or published date,
+        by filtering against a `author` and `published_date` query parameters in the URL.
+        """
+        queryset = Book.objects.all()
+        authors = self.request.query_params.getlist('author', None)
+        published_date = self.request.query_params.get('published_date', None)
+        if published_date:
+            queryset = queryset.filter(published_date=published_date)
+        if authors:
+            queryset_list = [queryset.filter(authors__icontains=author) for author in authors if author is not None]
+            return QuerySet.union(*queryset_list)
+        return queryset
 
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
